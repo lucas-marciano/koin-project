723a5f89201c3a95468ab93cc055cbb00c35583b
lucas-marciano <lucas.almeida.marciano@gmail.com>
2019-02-15 13:20:35 -0200

    Insert presentation folder

---
+++ app/src/main/java/br/com/lucasmarciano/koinproject/presentation/CurrencyViewModel.kt
@@ -0,0 +1,25 @@
+package br.com.lucasmarciano.koinproject.presentation
+
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+import br.com.lucasmarciano.koinproject.data.DataRepositoryFactory
+import br.com.lucasmarciano.koinproject.model.Currency
+
+/**
+ * [Description]
+ *
+ * @project KoinProject
+ * @create_at 15/02/19
+ * @author lucasmarciano
+ */
+class CurrencyViewModel(private val dataRepositoryFactory: DataRepositoryFactory) : ViewModel() {
+
+    private val currencyLiveData = MutableLiveData<List<Currency>>()
+
+    fun observerCurrencies() = currencyLiveData
+
+    fun retrieveCurrencies(json: String){
+        val data = dataRepositoryFactory.retrieveLocalSource().getCurrencies(json)
+        currencyLiveData.postValue(data)
+    }
+}
